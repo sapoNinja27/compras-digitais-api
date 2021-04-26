4b7b94d90446ce6585e56d7703add07c8e68ef8e
Joao Pedro Ronzani dos Santos <joao.ronzani@betha.com.br>
2021-04-26 10:07:39 -0300

    insert das categorias automatico

--- src/main/java/main/McApplication.java
+++ src/main/java/main/McApplication.java
@@ -1,13 +1,30 @@
 package main;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class McApplication {
+import main.domain.Categoria;
+import main.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class McApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1=new Categoria(null, "Informatica");
+		Categoria cat2=new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
